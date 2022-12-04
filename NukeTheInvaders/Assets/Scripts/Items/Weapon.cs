c0d3be69cdf7cf5c0a3b48e5dd2d08ec9f101c05
James Geer <jamesrgeer@hotmail.co.uk>
2022-12-04 13:56:03 +0000

    refactoring weapon class to fire ammo class

--- NukeTheInvaders/Assets/Scripts/Items/Weapon.cs
+++ NukeTheInvaders/Assets/Scripts/Items/Weapon.cs
@@ -5,36 +5,53 @@ using TMPro;
 
 public class Weapon : MonoBehaviour
 {
-    public GameObject rocketObj; //link to the rocket prefab
-    public TextMeshProUGUI currentAmmoText; //link to the rocket prefab
-    public TextMeshProUGUI maxAmmoText; //link to the rocket prefab
-    public int noRocket = 10; //number of rockets possessed
-    public int maxAmmo = 24;
-    public AudioClip shootSound; //rocket launch sound
-    public float maxSpeed= 60;//max rocket speed
-    public float rocketAcceleration = 1;
+    //public GameObject rocketObj; //link to the rocket prefab
+    // public TextMeshProUGUI currentAmmoText; //link to the rocket prefab
+    // public TextMeshProUGUI maxAmmoText; //link to the rocket prefab
+    // public int noRocket = 10; //number of rockets possessed
+    // public AudioClip shootSound; 
+
+    private Ammo _ammo;
+    
+    // amount of ammo
+    [SerializeField] private int ammoQuantity = 0;
+    
+    // rocket launch sound
+    [SerializeField] public AudioClip shootSound;
 
     private void Start()
     {
+        _ammo = GetComponent<Ammo>();
         // currentAmmoText.text = noRocket.ToString();
         // maxAmmoText.text = "/ " + maxAmmo.ToString();
+    }
+
+    public void LoadAmmo(int amount)
+    {
+        
     }
     
     // Update is called once per frame
     void Update()
     {
+        Shoot();
+    }
+    
+    private void Shoot()
+    {
         //if left control (fire1) pressed, and we still have at least 1 cell
-        if (Input.GetButtonDown("Fire1") && noRocket > 0)
+        if (Input.GetButtonDown("Fire1") && ammoQuantity > 0)
         {
-            noRocket--; //reduce the number of rockets
-            currentAmmoText.text = noRocket.ToString();
-            //play shooting sound
+            // decrement ammo count
+            ammoQuantity--; 
+
+            // play shooting sound
             AudioSource.PlayClipAtPoint(shootSound, transform.position);
             
-            //instantiate the rocket as game object (also make it point forward)
-            GameObject rocket = Instantiate(rocketObj, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f)) as GameObject;
-            //ask physics engine to ignore collison between power cell and our FPSController
+            // instantiate the rocket as game object (also make it point forward)
+            GameObject rocket = Instantiate(rocketObj, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
             
+            // ask physics engine to ignore collison between power cell and our FPSController
             Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), rocket.GetComponent<Collider>(), true);
         }
     }
