8b5089b79a65d846685adc703b10254b9521473a
James Geer <jamesrgeer@hotmail.co.uk>
2022-12-06 15:51:51 +0000

    infinite ammo replenishes one every 10secs

--- NukeTheInvaders/Assets/Scripts/Items/Weapon.cs
+++ NukeTheInvaders/Assets/Scripts/Items/Weapon.cs
@@ -5,9 +5,10 @@ using TMPro;
 
 public class Weapon : MonoBehaviour
 {
-    // which object this weapon will be firing
-    [SerializeField] private GameObject ammoObject;
-    
+    // ammo objects that this weapon will be firing
+    [SerializeField] private GameObject RegularAmmoObject;
+    [SerializeField] private GameObject InfiniteAmmoObject;
+
     // which ammo this weapon can use
     [SerializeField] private InventoryItem ammoType;
 
@@ -17,6 +18,7 @@ public class Weapon : MonoBehaviour
     private GameObject _playerGameObject;
     private Player _player;
     private Inventory _inventory;
+    private InventoryItem _infiniteAmmo;
 
     private void Start()
     {
@@ -28,30 +30,51 @@ public class Weapon : MonoBehaviour
         
         // player inventory
         _inventory = _player.Inventory;
+        
+        // infinite ammo
+        if (_player.InfiniteAmmo)
+        {
+            _infiniteAmmo = _player.InfiniteAmmo;
+        }
     }
     
-    // Update is called once per frame
+    // listen for trigger events (e.g. clicking the mouse to shoot)
     void Update()
     {
-        Shoot();
+        TriggerEvent();
     }
     
-    private void Shoot()
+    private void TriggerEvent()
     {
+        // left mouse click
         if (Input.GetButtonDown("Fire1"))
         {
+            // if player has any regular ammo, shoot that
             if (_inventory.TakeFromInventory(ammoType, 1))
             {
-                // play shooting sound
-                AudioSource.PlayClipAtPoint(shootSound, transform.position);
-                
-                // instantiate the projectile as game object (also make it point forward)
-                // GameObject projectile = Instantiate(ammoObject, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
-                Instantiate(ammoObject, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
-                
-                // ask physics engine to ignore collison between power cell and our FPSController
-                //Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), projectile.GetComponent<Collider>(), true);
+                FireAmmo(RegularAmmoObject);
+            }
+            // out of regular ammo, see if they have any infinite, replenishing ammo to use
+            else
+            {
+                if (_infiniteAmmo && _inventory.TakeFromInventory(_infiniteAmmo, 1))
+                {
+                    FireAmmo(InfiniteAmmoObject);
+                }
             }
         }
+    }
+
+    private void FireAmmo(GameObject ammoGameObject)
+    {
+        // play shooting sound
+        AudioSource.PlayClipAtPoint(shootSound, transform.position);
+                
+        // instantiate the projectile as game object (also make it point forward)
+        // GameObject projectile = Instantiate(ammoObject, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
+        Instantiate(ammoGameObject, transform.position, transform.rotation * Quaternion.Euler(90f, 0f, 0f));
+                
+        // ask physics engine to ignore collison between power cell and our FPSController
+        //Physics.IgnoreCollision(transform.root.GetComponent<Collider>(), projectile.GetComponent<Collider>(), true);
     }
 }
--- NukeTheInvaders/Assets/Scripts/Player/Player.cs
+++ NukeTheInvaders/Assets/Scripts/Player/Player.cs
@@ -9,11 +9,14 @@ public class Player : MonoBehaviour
 	private InventoryHolder _inventoryHolder;
 	private Inventory _inventory;
 	
+	[SerializeField] private InventoryItem infiniteAmmo;
 	[SerializeField] private InventoryItem startItem;
 	[SerializeField] private int startItemAmount;
 
 	// player inventory getter
 	public Inventory Inventory => _inventory;
+
+	public InventoryItem InfiniteAmmo => infiniteAmmo;
 	
 	/**
 	 * must be awake as otherwise when classes like the gun
@@ -21,16 +24,31 @@ public class Player : MonoBehaviour
 	 */
 	private void Awake()
 	{
-        // currentAmmoText.text = noRocket.ToString();
-        // maxAmmoText.text = "/ " + maxAmmo.ToString();
-
-        _inventoryHolder = GetComponent<InventoryHolder>();
+		_inventoryHolder = GetComponent<InventoryHolder>();
         _inventory = _inventoryHolder.Inventory;
+
+        if (infiniteAmmo)
+        {
+	        _inventory.AddToInventory(infiniteAmmo, 10);
+        }
 
         // see if a starting item has been assigned to the player
         if (startItem != null && startItemAmount > 0)
         {
 	        _inventory.AddToInventory(startItem, startItemAmount);
         }
+	}
+
+	private void Start()
+	{
+		InvokeRepeating(nameof(RefillInfiniteAmmo), 10f, 10f);
+	}
+
+	/**
+	 * slowly refill infinite ammo
+	 */
+	private void RefillInfiniteAmmo()
+	{
+		_inventory.AddToInventory(infiniteAmmo, 1);
 	}
 }
