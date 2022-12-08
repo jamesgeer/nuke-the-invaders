65e0bf50e293bd1ca64435e5a28cc8228fd60bc3
James Geer <jamesrgeer@hotmail.co.uk>
2022-12-08 13:57:01 +0000

    comments and can afford method

--- NukeTheInvaders/Assets/Scripts/Player/PlayerTokens.cs
+++ NukeTheInvaders/Assets/Scripts/Player/PlayerTokens.cs
@@ -6,15 +6,30 @@ public class PlayerTokens : MonoBehaviour
 {
     private int tokensHeld = 0;
 
+    // tokens held getter
     public int TokensHeld => tokensHeld;
     
+    /**
+     * increase amount of tokens held by player
+     */
     public void IncreaseTokens(int amount)
     {
         tokensHeld += amount;
     }
 
+    /**
+     * decrease number of tokens held by player
+     */
     public void DecreaseTokens(int amount)
     {
         tokensHeld -= amount;
+    }
+
+    /**
+     * check if player can afford this purchase
+     */
+    public bool CanAfford(int amountToDecrease)
+    {
+        return tokensHeld - amountToDecrease < 1;
     }
 }
