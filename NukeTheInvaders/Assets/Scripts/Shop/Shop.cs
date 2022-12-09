c3452ad5e4401944e3c654f9093aebd2a6eccbc7
James Geer <jamesrgeer@hotmail.co.uk>
2022-12-09 01:42:26 +0000

    shop inventory opens/closes when entering/leaving shop box collider

--- NukeTheInvaders/Assets/Scripts/Shop/Shop.cs
+++ NukeTheInvaders/Assets/Scripts/Shop/Shop.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Shop : MonoBehaviour
 {
     private ShopInventory shopInventory;
-
+    
+    [SerializeField] private GameObject shopInventoryObject;
     [SerializeField] private List<ShopInventoryItem> shopItems;
 
     /**
@@ -26,6 +30,27 @@ public class Shop : MonoBehaviour
                 shopInventory.AddToInventory(shopItem);
             }
         }
+    }
 
+    /**
+     * display shop inventory when player is in proximity of the shop
+     */
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            shopInventoryObject.SetActive(true);
+        }
+    }
+    
+    /**
+     * hide shop inventory when player leaves the shop zone
+     */
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            shopInventoryObject.SetActive(false);
+        }
     }
 }
